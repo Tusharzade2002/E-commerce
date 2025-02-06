7828376a4aa2265aa335701ed5e334823bf6abce
Tusharzade <tusharzade29@gmail.com>
2025-02-06 16:52:03 +0530

    routing implement

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-
+import { RouterProvider , createBrowserRouter } from 'react-router-dom';
+import Login from './Views/Login';
+import Signup from './Views/Signup';
+import NotFound from './Views/NotFound';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
+router=createBrowserRouter([
+     {
+      path:"/",
+      element:<Home />
+     },
+     {
+      path:"/login",
+      element:<Login/>
+     },
+     {
+      path:"/signup",
+      element:<Signup/>
+     },
+     {
+      path:"*",
+      element:<NotFound/>
+     }
+])
 root.render(
 <> 
-  
+   <RouterProvider router={router} />
 </>
 );
 
